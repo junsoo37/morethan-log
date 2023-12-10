bbd900f385e597584cdcb330f357316fab2a5768
junsoo37 <junsoor37@gmail.com>
2023-12-09 20:41:41 -0800

    Edit content

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   // Current num of posts: 6
   link: "https://www.junsoor.com",
   since: 2023, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "en-US", //['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
