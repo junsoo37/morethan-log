80748ec40dd0e1fd8b8071c98e00ea3fb218552c
junsoo37 <junsoor37@gmail.com>
2024-08-13 23:55:04 -0400

    cache vercel edge network

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   i18n: {
     locales: ['en', 'ko'],
     defaultLocale: 'ko',
@@ -6,4 +8,19 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
+ 
+module.exports = nextConfig;
